63cc39a7ee1a25a41c6b41604f024d29b0303adb
MinhoYoo <minhoyoo1899@gmail.com>
2022-10-20 18:10:21 +0900

    connect to db

---
+++ express-mysql-example/index.js
@@ -0,0 +1,63 @@
+const { query } = require('express');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  port: "8282",
+  password: 'TheoHernandez19!',
+  database: 'my_db'
+});
+
+// console.log(connection);
+// console.log("=================asdsadasdasdas==============");
+// connection.query = "anbasdsadasd";
+// console.log(connection.query);
+//console.log(query);
+// connection.connect();
+
+connection.query('SELECT * from Users', (error, rows, fields) => {
+  if (error) throw error;
+  console.log('User info is : ', rows);
+});
+
+connection.end();
+
+
+
+
+//================================================
+
+// const express = require('express');
+// const mysql      = require('mysql');
+// const dbconfig   = require('./config/database.js');
+// const connection = mysql.createConnection(dbconfig);
+
+// const app = express();
+
+// // configuration =========================
+// app.set('port', process.env.PORT || 3000);
+
+// app.get('/', (req, res) => {
+//   res.send('Root');
+// });
+
+// app.get('/users', (req, res) => {
+//   connection.query('SELECT * from Users', (error, rows) => {
+//     if (error) throw error;
+//     console.log('User info is: ', rows);
+//     res.send(rows);
+//   });
+// });
+
+// app.listen(app.get('port'), () => {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+
+// // config/database.js
+// module.exports = {
+//   host     : 'localhost',
+//   user     : '< MySQL username >',
+//   password : '< MySQL password >',
+//   database : 'my_db'
+// };
